4faa085b83ed00b0ecb6d1bcd06677cdedf56d40
vinitjha1993 <33178021+vinitjha1993@users.noreply.github.com>
2020-02-20 15:09:32 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,29 +1,4 @@
-provider "aws" {
-  assume_role {
-    role_arn     = "role"
-  }
-}
-
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = <<EOF
-  {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": ["*"],
-          "Effect": "Allow",
-          "Resource": "*"
-        }
-      ]
-    }
-  EOF
-
-  tags = {
-    Name = "iam-role-atlantis"
-  }
+resource "null_resource" "cluster" {
 }
 
 
