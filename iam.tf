475557e1db0570ee2d6ef16370a2eb2e6cadd397
vinit kumar jha <vinitkrjha93@gmail.com>
2020-02-20 12:50:58 +0530

    test atlantis-1

---
+++ iam.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  assume_role {
+    role_arn     = "${var.role_arn}"
+  }
+}
+
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name = "iam-role-atlantis"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "role_arn" {
+  type = "string"
+}
+
